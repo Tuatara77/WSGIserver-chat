6b8ca998d1fddc211e54fa09acc01a916b10f592
Tuatara77 <44623766+Tuatara77@users.noreply.github.com>
2022-05-26 18:53:46 +0100

    changed message handling

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ import wsgiserver
 
 class Server:
     def __init__(self, port:int=8000):
+        self.sep = "|<SEPARATOR>"
         self.paths = wsgiserver.WSGIPathInfoDispatcher({
             "/": self._main,
             "/send": self._receive,
             "/get": self._send
         })
         self.server = wsgiserver.WSGIServer(self.paths, port=port)
-        self.data = None
+        self.data = []
     
     def start(self):
         """Starts the server"""
@@ -21,22 +22,21 @@ class Server:
 
     def _main(self, environ, start_response):
         """Main page, appears as a gateway error"""
-        body = b""
         start_response("503 ok", [('Content-type','text/plain')])
-        yield body
+        yield b""
     
     def _receive(self, environ, start_response):
         """Server is RECEIVING data"""
         data = environ["PATH_INFO"][1:]
         if data:
-            body = b""
-            self.data = data
+            self.data.append(data)
             start_response("503 ok", [('Content-type','text/plain')])
-            yield body
         else: start_response("503 ok", [('Content-type','text/plain')])
-        
+        yield b""
+
     def _send(self, environ, start_response):
         """Server is SENDING data"""
         start_response("503 ok", [("Content-type", "text/plain")])
-        yield bytes(f"{self.data}", "utf-8")
-
+        body = b""
+        for f in self.data: body += bytes(f"{f}{self.sep}", "utf-8")
+        yield body
