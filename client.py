0d30c3be80a07283790c61b7c2a18fd888adde5f
Tuatara77 <44623766+Tuatara77@users.noreply.github.com>
2022-05-27 14:41:49 +0100

    updated efficiency (probably?)

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
-import http.client
+from http.client import HTTPConnection
 
 class Client:
-    def __init__(self, server_ip:str, server_port:int=8000):
+    def __init__(self, server_ip:str, server_port:int=80):
         self.ip = server_ip
         self.port = server_port
 
     def request(self):
-        conn = http.client.HTTPConnection(f"{self.ip}:{self.port}", timeout=10)
+        conn = HTTPConnection(f"{self.ip}:{self.port}", timeout=10)
         conn.request("GET","/get")
         r1 = conn.getresponse()
         content = r1.read()
@@ -14,8 +14,7 @@ class Client:
         return str(content)[2:-1]
 
     def send(self, *data):
-        conn = http.client.HTTPConnection(f"{self.ip}:{self.port}", timeout=10)
-        path = "/send"
-        for f in data: path += f"/{f}"
+        conn = HTTPConnection(f"{self.ip}:{self.port}", timeout=10)
+        path = "/send" + "/".join(data)
         conn.request("SEND", path)
         conn.close()
